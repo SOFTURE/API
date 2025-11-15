16bf3c68ec7d0ebf64a2ccbba4e537c5c5eddd54
Jarosław Armatys <jarekarmatys@gmail.com>
2025-11-15 13:21:25 +0100

    Added SOFTURE.Common.CQRS project with middleware and validation behavior

---
+++ API/SOFTURE.Common.CQRS/Middlewares/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SOFTURE.Common.CQRS.Middlewares.Behaviours;
+
+namespace SOFTURE.Common.CQRS.Middlewares
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddMiddlewares(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CommandValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
