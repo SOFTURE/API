a3b8634a144651de397f1d3147e647a7aefc0bf0
Jarosław Armatys <jarekarmatys@gmail.com>
2025-11-20 09:08:26 +0100

    fix: Correct input handling in GuidParser and NumberParser methods

--- API/SOFTURE.Common.StronglyTypedIdentifiers/API/Parsers/IdentifierParsers.cs
+++ API/SOFTURE.Common.StronglyTypedIdentifiers/API/Parsers/IdentifierParsers.cs
@@ -9,7 +9,7 @@ public static class IdentifierParsers
     public static ParseResult GuidParser<TIdentifier>(StringValues input)
         where TIdentifier : IIdentifier
     {
-        var success = Guid.TryParse(input?.ToString(), out var result);
+        var success = Guid.TryParse(input, out var result);
         
         var identifier = (TIdentifier)Activator.CreateInstance(typeof(TIdentifier), result)!;
         
@@ -19,7 +19,7 @@ public static class IdentifierParsers
     public static ParseResult NumberParser<TIdentifier>(StringValues input)
         where TIdentifier : IIdentifier
     {
-        var success = int.TryParse(input?.ToString(), out var result);
+        var success = int.TryParse(input, out var result);
         
         var identifier = (TIdentifier)Activator.CreateInstance(typeof(TIdentifier), result)!;
         
