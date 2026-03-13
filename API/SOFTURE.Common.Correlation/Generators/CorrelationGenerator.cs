c88c910319bf071011c19c8339a83ef435a2f9b9
Jarosław Armatys <jarekarmatys@gmail.com>
2026-03-13 15:33:44 +0100

    refactor: Update target frameworks to include .NET 10.0 and adjust package references

--- API/SOFTURE.Common.Correlation/Generators/CorrelationGenerator.cs
+++ API/SOFTURE.Common.Correlation/Generators/CorrelationGenerator.cs
@@ -6,16 +6,20 @@ public static class CorrelationGenerator
 {
     public static CorrelationId Generate()
     {
-# if NET9_0
+#if NET10_0
         return new CorrelationId(Guid.CreateVersion7());
-# endif
-        
+#endif
+
+#if NET9_0
+        return new CorrelationId(Guid.CreateVersion7());
+#endif
+
 #if NET8_0
         return new CorrelationId(Guid.NewGuid());
-# endif
-        
+#endif
+
 #if NET6_0
         return new CorrelationId(Guid.NewGuid());
-# endif
+#endif
     }
 }
